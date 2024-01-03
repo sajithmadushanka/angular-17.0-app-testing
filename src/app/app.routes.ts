4660f2d5139c109ee13dc34b73e0b54a97497d90
sajeebro <sajithmadusanka720@gmail.com>
2024-01-04 00:43:36 +0530

    completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./componets/home/home.component";
+import {AboutComponent} from "./componets/about/about.component";
+import {ProductComponent} from "./componets/product/product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:'home',pathMatch:"full"},
+  {path:'home', component:HomeComponent},
+  {path:'shop', component:ProductComponent},
+  {path:'about', component: AboutComponent}
+];
